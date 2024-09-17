d925d8c1d019af98703503ac246aacc052d70916
Kunalbhattarai911 <Kunalbhattarai911@gmail.com>
2024-09-17 17:26:14 +0545

    feat | created API for google login / signup

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -43,51 +43,139 @@ export const signup = async (req, res) => {
   }
 };
 
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-export const login = async (req,res) => {
-    try {
-        const {email, password} = req.body;
-
-       
-        const user = await User.findOne({email})
-        if(!user){
-            return res.status(404).json({
-                message : "Email or Password is incorrect",
-                success : false
-            })
-        }
-
-        const isPasswordCorrect = await bcryptjs.compare(password, user.password)
-        if(!isPasswordCorrect){
-            return res.status(404).json({
-                message : "Email or Password is incorrect",
-                success :false
-            })
-        }
-
-        const token = jwt.sign(
-            { email: user.email, _id: user._id }, 
-            process.env.SECRET_KEY,
-            { expiresIn: '24h' }
-        )
-
-        const userDetails = {
-            _id : user._id,
-            username : user.username,
-            email : user.email
-        }
-
-        return res.status(200).cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpsOnly: true, sameSite: 'strict' }).json({
-            message: "Login Successful",
-            success: true,
-            token,
-            userDetails
-        })
-    } catch (error) {
-        console.log("Error Details:", error);
-        return res.status(500).json({
-            message : "An Error Occured While Logging the User",
-            success : false
-        })
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({
+        message: "Email or Password is incorrect",
+        success: false,
+      });
     }
-}
+
+    const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(404).json({
+        message: "Email or Password is incorrect",
+        success: false,
+      });
+    }
+
+    const token = jwt.sign(
+      { email: user.email, _id: user._id },
+      process.env.SECRET_KEY,
+      { expiresIn: "24h" }
+    );
+
+    const userDetails = {
+      _id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+        httpsOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        message: "Login Successful",
+        success: true,
+        token,
+        userDetails,
+      });
+  } catch (error) {
+    console.log("Error Details:", error);
+    return res.status(500).json({
+      message: "An Error Occured While Logging the User",
+      success: false,
+    });
+  }
+};
+
+
+export const google = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    if (user) {
+      const token = jwt.sign(
+        { email: user.email, _id: user._id },
+        process.env.SECRET_KEY,
+        { expiresIn: "24h" }
+      );
+
+      const userDetails = {
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+      };
+
+      return res
+        .status(200)
+        .cookie("token", token, {
+          maxAge: 1 * 24 * 60 * 60 * 1000,
+          httpsOnly: true,
+          sameSite: "strict",
+        })
+        .json({
+          message: "Login Successful",
+          success: true,
+          token,
+          userDetails,
+        });
+    } else {
+      const generatePassword =
+        Math.random().toString(32).slice(-8) +
+        Math.random().toString(32).slice(-8);
+      const hashedPassword = await bcryptjs.hash(generatePassword, 10);
+
+      const newUser = new User({
+        username:
+          req.body.name.split(" ").join("").toLowerCase() +
+          Math.random().toString(32).slice(-4),
+        email: req.body.email,
+        password: hashedPassword,
+        avatar: req.body.photo,
+      });
+
+      await newUser.save();
+
+      const token = jwt.sign(
+        { email: newUser.email, _id: newUser._id },
+        process.env.SECRET_KEY,
+        { expiresIn: "24h" }
+      );
+
+      const userDetails = {
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+      };
+
+      return res
+        .status(200)
+        .cookie("token", token, {
+          maxAge: 1 * 24 * 60 * 60 * 1000,
+          httpsOnly: true,
+          sameSite: "strict",
+        })
+        .json({
+          message: "Login Successful",
+          success: true,
+          token,
+          userDetails,
+        });
+    }
+  } catch (error) {
+    console.log("Error Details:", error);
+    return res.status(500).json({
+      message: "An Error Occured While login or signup using Google.",
+      success: false,
+    });
+  }
+};
--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { login, signup } from '../controllers/auth.controller.js';
+import { google, login, signup } from '../controllers/auth.controller.js';
 import { authLogin, authSignup } from '../validation/auth.validation.js';
 
 const router = express.Router();
 
 router.post("/signup" ,authSignup, signup);
-router.post("/login", authLogin, login)
+router.post("/login", authLogin, login);
+router.post("/google", google);
 
 export default router;
