c9fa95d30c68c38030092279dfabe43d203b63ba
Kunalbhattarai911 <Kunalbhattarai911@gmail.com>
2024-09-17 09:40:51 +0545

    Feat | created index.js file and connected to database

---
+++ Backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Successfully connected to Database.");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on the PORT ${PORT}`);
+});
