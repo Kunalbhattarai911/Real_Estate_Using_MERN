86e5c34d99d8b49dd438c902209b9a363546aab0
Kunalbhattarai911 <Kunalbhattarai911@gmail.com>
2024-09-19 22:51:38 +0545

    Feat | Created API for Get User Listing

--- Backend/controllers/listing.controller.js
+++ Backend/controllers/listing.controller.js
@@ -1,21 +1,75 @@
-import Listing from "../models/listing.model.js"
+import Listing from "../models/listing.model.js";
 
+export const createListing = async (req, res) => {
+  try {
+    const {
+      name,
+      type,
+      description,
+      address,
+      regularPrice,
+      discountPrice,
+      bathrooms,
+      bedrooms,
+      furnished,
+      parking,
+      offer,
+      imageUrls,
+      userRef,
+    } = req.body;
 
-export const createListing = async(req,res) => {
-    try {
-        const listing = await Listing.create(req.body)
+    const userId = req.id;
 
-        return res.status(201).json({
-            message : "New Listing Added Successfully",
-            success : true,
-            listing
-        })
-    } catch (error) {
-        console.log("Error Details:", error);
-        return res.status(500).json({
-            message : "An Error Occured while adding listing",
-            Error : error.message,
-            success : false
-        })
-    }
-}
+    const listing = await Listing.create({
+      userId,
+      name,
+      type,
+      description,
+      address,
+      regularPrice,
+      discountPrice,
+      bathrooms,
+      bedrooms,
+      furnished,
+      parking,
+      offer,
+      imageUrls,
+      userRef,
+    });
+
+    return res.status(201).json({
+      message: "New Listing Added Successfully",
+      success: true,
+      listing,
+    });
+  } catch (error) {
+    console.log("Error Details:", error);
+    return res.status(500).json({
+      message: "An Error Occured while adding listing",
+      Error: error.message,
+      success: false,
+    });
+  }
+};
+
+export const getUserListing = async (req, res) => {
+  try {
+    const userId = req.id;
+    //   console.log("User ID in getUserListing:", userId);
+
+    const userListing = await Listing.find({ userId });
+
+    return res.status(200).json({
+      message: "User Listings retrieved successfully",
+      success: true,
+      userListing,
+    });
+  } catch (error) {
+    console.log("Error Details:", error);
+    return res.status(500).json({
+      message: "An Error Occured while retrieving the listing of this user",
+      Error: error.message,
+      success: false,
+    });
+  }
+};
--- Backend/routes/listing.route.js
+++ Backend/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { isAuthenticated } from "../middlewares/user.isAuthenticated.js";
 import { listingValidation } from "../validation/listing.validation.js";
-import { createListing } from "../controllers/listing.controller.js";
+import { createListing, getUserListing } from "../controllers/listing.controller.js";
 
 const router = express.Router();
 
 router.post("/create", isAuthenticated, listingValidation , createListing)
+router.get("/getlisting", isAuthenticated, getUserListing)
 
 export default router;
